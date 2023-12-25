535248ff699cb95c898be2dbf09508bf95d2c10b
MarcoDuque <marcosduquesomatic@gmail.com>
2023-12-24 21:29:39 -0500

    Basic routes design

--- AlienFrontend/src/app/app.routes.ts
+++ AlienFrontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './screens/customers/customers.component';
+import { CoursesComponent } from './screens/courses/courses.component';
+import { BillsComponent } from './screens/bills/bills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': 'customers', component: CustomersComponent },
+    {'path': 'courses', component: CoursesComponent },
+    {'path': 'bills', component: BillsComponent }
+];
