81aaf10b6e1fa3b0dd16c8d66bed7880fa7120bc
MarcoDuque <marcosduquesomatic@gmail.com>
2024-02-26 19:41:56 -0500

    name change

---
+++ ShlomoFrontend/src/app/constants/constants.ts
@@ -0,0 +1,9 @@
+export const navBar = {
+    clientes: 'Clientes',
+    Store: 'Clientes',
+    Courses: 'Clientes',
+    Bills: 'Clientes',
+    Reports: 'Clientes',
+    Seettings: 'Clientes',
+    LogOut: 'Clientes'
+}
